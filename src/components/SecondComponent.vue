88c69af62fe5d8bee5668608c8434413e45a526a
jayhong329 <smoja329@gmail.com>
2024-09-26 21:20:11 +0800

    TryCompositionAPI-Ref

--- src/components/SecondComponent.vue
+++ src/components/SecondComponent.vue
@@ -7,7 +7,7 @@ const message = ref("響應式互動")
 <template>
     <div>
         <h2>My Second Component 這是第二個組件</h2>
-        <p>Composition APIs</p>
+        <p>Composition API</p>
         <input type="text" v-model="message">
         <p>{{ message }}</p>
     </div>
